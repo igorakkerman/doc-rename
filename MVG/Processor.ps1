df783075c7f89b24b6e4e8782c4b197ae5516b26
Igor Akkerman <commit@igorakkerman.de>
2019-11-09 12:57:12 +0100

    MVG invoice parsing adapted to format change

--- MVG/Processor.ps1
+++ MVG/Processor.ps1
@@ -3,25 +3,41 @@
     
     $textContent = pdftotext.exe -enc UTF-8 -bom ${filename} - | Out-String
 
-    if ( ${textContent} -cmatch "(?s).*Rechnungsnummer:\W*(MVG)?([A-Z0-9]+).*") {
+    if ( ${textContent} -cmatch "(?s).*Rechnungsnummer:\W*(MVG)?([A-Z0-9-]+).*") {
         $invoiceNumber = $matches[1] + $matches[2]
     }
-    if ( ${textContent} -cmatch "(?s).*Rechnungsnummer:\W*(?:MVG)?([0-9][0-9][0-9][0-9])([0-9][0-9])([0-9][0-9]).*") {
+    if ( ${textContent} -cmatch "(?s).*Bestellung vom ([0-9]+)\. ([A-Za-zä]+) ([0-9][0-9][0-9][0-9]).*") {
     
-        $invoiceYear = $matches[1]
-        $invoiceMonth = $matches[2]
-        $invoiceDay = $matches[3]
+        $invoiceDay = "{0:00}" -f [int]$matches[1]
+        $invoiceMonthText = $matches[2]
+        $invoiceYear = $matches[3]
+
+        $invoiceMonth = switch ($invoiceMonthText) {
+            "Januar" { "01" }
+            "Februar" { "02" }
+            "März" { "03" }
+            "April" { "04" }
+            "Mai" { "05" }
+            "Juni" { "06" }
+            "Juli" { "07" }
+            "August" { "08" }
+            "September" { "09" }
+            "Oktober" { "10" }
+            "November" { "11" }
+            "Dezember" { "12" }
+            Default { "ERROR" }
+        }
 
         $invoiceDate = ${invoiceYear} + "-" + ${invoiceMonth} + "-" + ${invoiceDay}
     }
     
-    if ( ${textContent} -cmatch "(?s).*Summe\W*([0-9]+,[0-9][0-9]).*") { 
+    if ( ${textContent} -cmatch "(?s).*Brutto Gesamt:.*(?:Summe)?\W+([0-9]+,[0-9][0-9]).*") { 
         $invoiceAmount = $matches[1] 
     }
     
     # Write-Host "${filename}: ${invoiceNumber} ${InvoiceDate} ${InvoiceAmount}"
     if (-not ${invoiceNumber} -or -not ${invoiceDate} -or -not ${invoiceAmount}) {
-        Write-Host "Invalid data. Ignoring ${filename}"
+        Write-Output "Invalid data. Ignoring ${filename}"
         Return
     }
 
@@ -32,13 +48,13 @@
         $newFilename = "${invoiceDate} 0${index} Rechnung ${invoiceNumber} ${invoiceAmount}€.pdf"
 
         if (${newFilename} -eq ${filename}) {
-            Write-Host "File has correct name. Ignoring ${filename}"
+            Write-Output "File has correct name. Ignoring ${filename}"
             Return
         }  
     }
     while (Test-Path "${invoiceDate} 0${index}*")
 
-    Write-Host "Renaming '${filename}' to '${newFilename}'"
+    Write-Output "Renaming '${filename}' to '${newFilename}'"
     Rename-Item -Path "${filename}" -NewName ${newFilename}
 }
 
