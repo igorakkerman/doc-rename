86e7f5d77630f50bdf5061fcba4e1caf4252d534
Igor Akkerman <commit@igorakkerman.de>
2019-12-17 21:36:07 +0100

    Barclaycard Visa statement processor created

---
+++ Barclaycard/Processor.ps1
@@ -0,0 +1,65 @@
+﻿Get-ChildItem -filter *.pdf | Where-Object {$_.LastWriteTime -ge "2019-01-01"} | ForEach-Object {
+    $filename = $_.Name
+
+    $textContent = pdftotext -raw -enc UTF-8 -bom -q ${filename} - | Out-String
+
+    if (${textContent} -NotMatch "service@barclaycard.de") {
+        Write-Host "No Barclaycard statement. Skipping ${filename}"
+        Return
+    }
+
+    if ( ${textContent} -cmatch "(?s).*Kontoübersicht vom ([0-9]+)\. ([A-Za-zä]+) ([0-9][0-9][0-9][0-9]).*") {
+    
+        $statementDay = "{0:00}" -f [int]$matches[1]
+        $statementMonthText = $matches[2]
+        $statementYear = $matches[3]
+
+        $statementMonth = switch ($statementMonthText) {
+            "Januar" { "01" }
+            "Februar" { "02" }
+            "März" { "03" }
+            "April" { "04" }
+            "Mai" { "05" }
+            "Juni" { "06" }
+            "Juli" { "07" }
+            "August" { "08" }
+            "September" { "09" }
+            "Oktober" { "10" }
+            "November" { "11" }
+            "Dezember" { "12" }
+            Default { "ERROR" }
+        }
+
+        $statementDate = ${statementYear} + "-" + ${statementMonth} + "-" + ${statementDay}
+    }
+    
+    $statementAmount = ${textContent} -replace "(?s).*Neuer Saldo [^\n]+ ([0-9.,]+)(\+?)-?\r\n.*", '$2$1'
+    if ( ${textContent} -cmatch "(?s).*Gesamt \(EUR\) [^\n]+ ([0-9.,]+).*" ) {
+        $payAmount = $matches[1]
+        $payString = " einzahlen ${payAmount}€"
+    }
+    else {
+        $payString = ""
+    }
+    $newFilename = "${statementDate} 01 Kontoübersicht ${statementAmount}€${payString}.pdf"
+
+    # Write-Host "Date:   $statementDate"
+    # Write-Host "Amount: $statementAmount"
+    # Write-Host "Pay:    $payAmount"
+    # Write-Host "OLD:    $filename"
+    # Write-Host "NEW:    $newFilename"
+
+    if (-not ${statementDate} -or -not ${statementAmount}) {
+        Write-Host "Missing values. Skipping ${filename}"
+        Return
+    }
+
+    if (${newFilename} -eq ${filename}) {
+        Write-Host "Correct name. Skipping ${filename}"
+        Return
+    }
+
+    Write-Host "Renaming '${filename}' to '${newFilename}'"
+    Rename-Item -Path "${filename}" -NewName ${newFilename}
+}
+
