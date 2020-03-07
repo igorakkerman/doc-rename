ed6ad0e2706de0847b8f5424f6abf75c5aae4fc6
Igor Akkerman <commit@igorakkerman.de>
2020-03-07 23:57:05 +0100

    SIXT share: handling documents without "Übergabe" date and OCR extracted amount

--- SIXT share/Processor.ps1
+++ SIXT share/Processor.ps1
@@ -10,8 +10,13 @@
 
     $invoiceNumber = ${textContent} -replace "(?s).*Rechnungsnr.\W+([0-9A-Z/]+).*", '$1'
     $invoiceDate =   ${textContent} -replace "(?s).*Übergabe:\W+([0-9][0-9])\.([0-9][0-9])\.([0-9][0-9][0-9][0-9]).*", '$3-$2-$1'
-#     $invoiceAmount = ${textContent} -replace "(?s).*\W([0-9]+,[0-9][0-9]) EUR.*", '$1'
-
+    if ($invoiceDate.Length -ne 10) {
+        $invoiceDate = ${textContent} -replace "(?s).*Rechnungsdatum:\W+Pullach, ([0-9][0-9])\.([0-9][0-9])\.([0-9][0-9][0-9][0-9]).*", '$3-$2-$1'
+    }
+    $invoiceAmount = ${textContent} -replace "(?s).*SIXT share Fahrt\W*\r\n\W*([0-9]+),\W*([0-9][0-9]) EUR.*", ' $1,$2€'
+    if ($invoiceAmount.Length -lt 5 -or $invoiceAmount.Length -gt 7) {
+        $invoiceAmount = ""
+    }
 
     if ([string]::IsNullOrWhiteSpace($invoiceNumber)) {
         Write-Host "Ignoring   ${filename}"
@@ -20,7 +25,7 @@
 
     $invoiceNumberClean = ${invoiceNumber} -replace "/", "_"
 
-    $newFilename = "${invoiceDate} 01 Rechnung SIXT share ${invoiceNumberClean}.pdf"
+    $newFilename = "${invoiceDate} 01 Rechnung SIXT share ${invoiceNumberClean}${invoiceAmount}.pdf"
 
     # Write-Host "Number: $invoiceNumber"
     # Write-Host "Date:   $invoiceDate"
