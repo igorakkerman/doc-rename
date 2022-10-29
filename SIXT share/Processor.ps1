bd933fb427ddc68705df9d02f94c5311c730c6c8
Igor Akkerman <commit@igorakkerman.de>
2022-10-30 01:03:15 +0200

    retrieval optimized, logging increased in SIXT Share Processor

--- SIXT share/Processor.ps1
+++ SIXT share/Processor.ps1
@@ -4,23 +4,24 @@
     $textContent = pdftotext -enc UTF-8 -bom -q ${filename} - | Out-String
 
     if (${textContent} -NotMatch "SIXT share") {
-        # Write-Host "Ignoring ${filename}"
+        Write-Verbose "Ignoring ${filename}"
         Return
     }
-    Write-Host "File $filename"
+    Write-Verbose "Processing file $filename"
 
     pause
 
     $documentType = if ($textContent -notmatch "Rechnungsstorno") {"Rechnung"} else {"Storno"}
-    $invoiceNumber = ${textContent} -replace "(?s).*?\r\n([0-9A-Z/]+?) Pullach,.*", '$1'
-    $invoiceDate = ${textContent} -replace "(?s).*?\r\n[0-9A-Z/]+? Pullach, ([0-9][0-9])\.([0-9][0-9])\.([0-9][0-9][0-9][0-9]).*", '$3-$2-$1'
+    $invoiceNumber = ${textContent} -replace "(?s).+?Rechnungsnr.\s+([0-9A-Z/]+).*", '$1'
+    $invoiceDate = ${textContent} -replace "(?s).+?Rechnungsdatum:\s+(?:Pullach, )([0-9][0-9])\.([0-9][0-9])\.([0-9][0-9][0-9][0-9]).*", '$3-$2-$1'
+    $invoiceAmount = ${textContent} -replace "(?s).+Übergabe:.+([0-9]+),\s?([0-9][0-9]) EUR\r\n.+", ' $1,$2€'
     $invoiceAmount = ${textContent} -replace "(?s).+\r\n([0-9]+),\s?([0-9][0-9]) EUR\r\n.+", ' $1,$2€'
     if ($invoiceAmount.Length -lt 6 -or $invoiceAmount.Length -gt 8) {
         $invoiceAmount = ""
     }
 
     if ([string]::IsNullOrWhiteSpace($invoiceNumber)) {
-        # Write-Host "Ignoring ${filename}"
+        Write-Verbose "Ignoring ${filename}"
         Return
     }
 
@@ -35,18 +36,21 @@
     $newFilename = "${invoiceDate} 0${index} ${documentType} SIXT share ${invoiceNumberClean}${invoiceAmount}.pdf"
 
     if (${newFilename} -eq ${filename}) {
-        # Write-Output "File has correct name. Ignoring ${filename}"
+        Write-Verbose "File has correct name. Ignoring ${filename}"
         Return
-    }  
+    }
 
 
-    # Write-Host "Number: $invoiceNumber"
-    # Write-Host "Date:   $invoiceDate"
-    # Write-Host "Amount: $invoiceAmount"
-    # Write-Host "OLD:    $filename"
-    # Write-Host "NEW:    $newFilename"
+    Write-Verbose "Number: $invoiceNumber"
+    Write-Verbose "Date:   $invoiceDate"
+    Write-Verbose "Amount: $invoiceAmount"
+    Write-Verbose "OLD:    $filename"
+    Write-Verbose "NEW:    $newFilename"
 
-    Write-Host "Renaming '${filename}' to '${newFilename}'"
-    Rename-Item -Path "${filename}" -NewName ${newFilename}
+    if ($newFilename -eq $filename) {
+        return
+    }
+
+    Write-Output "Renaming '${filename}' to '${newFilename}'"
+    Rename-Item -WhatIf -Path "${filename}" -NewName ${newFilename}
 }
-
