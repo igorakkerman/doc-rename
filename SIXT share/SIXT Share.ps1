a22b605e85d63811fb543864cb95f03b8d5c99f0
Igor Akkerman <commit@igorakkerman.de>
2022-10-30 18:44:19 +0100

    updated SIXT Share to new document version

--- SIXT share/SIXT Share.ps1
+++ SIXT share/SIXT Share.ps1
@@ -1,31 +1,34 @@
-﻿Get-ChildItem -filter "*.pdf" | Where-Object { $_.LastWriteTime -ge "2020-12-01" } | ForEach-Object {
+﻿# $VerbosePreference = "Continue"
+
+Get-ChildItem -filter "*.pdf" | ForEach-Object {
     $filename = $_.Name
 
-    $textContent = pdftotext -enc UTF-8 -bom -q ${filename} - | Out-String
+    $textContent = pdftotext -enc UTF-8 -bom -simple -q ${filename} - | Out-String
 
-    if (${textContent} -NotMatch "SIXT share") {
+    if (${textContent} -match "SIXT share") {
+        Write-Verbose "Old document version. Ignoring ${filename}"
+        Return
+    }
+
+    if (${textContent} -notmatch "Sixt GmbH & Co") {
         Write-Verbose "Ignoring ${filename}"
         Return
     }
     Write-Verbose "Processing file $filename"
 
-    pause
+    $documentType = if ($textContent -notmatch "Rechnungsstorno") {"Storno"} else {$null}
+    $invoiceNumber = ${textContent} -replace "(?s).+?Rechn.\W+Nr.:\s+(\d+).*", '$1'
+    $invoiceDate = ${textContent} -replace "(?s).+Pullach,\W+(\d{2})\.(\d{2}).(\d{4}).*", '$3-$2-$1'
+    $invoiceAmount = ${textContent} -replace "(?s).+Endbetrag.+(\d+),(\d{2})\W+EUR.+", ' $1,$2€'
 
-    $documentType = if ($textContent -notmatch "Rechnungsstorno") {"Rechnung"} else {"Storno"}
-    $invoiceNumber = ${textContent} -replace "(?s).+?Rechnungsnr.\s+([0-9A-Z/]+).*", '$1'
-    $invoiceDate = ${textContent} -replace "(?s).+?Rechnungsdatum:\s+(?:Pullach, )([0-9][0-9])\.([0-9][0-9])\.([0-9][0-9][0-9][0-9]).*", '$3-$2-$1'
-    $invoiceAmount = ${textContent} -replace "(?s).+Übergabe:.+([0-9]+),\s?([0-9][0-9]) EUR\r\n.+", ' $1,$2€'
-    $invoiceAmount = ${textContent} -replace "(?s).+\r\n([0-9]+),\s?([0-9][0-9]) EUR\r\n.+", ' $1,$2€'
-    if ($invoiceAmount.Length -lt 6 -or $invoiceAmount.Length -gt 8) {
-        $invoiceAmount = ""
-    }
+    Write-Verbose "Number: $invoiceNumber"
+    Write-Verbose "Date:   $invoiceDate"
+    Write-Verbose "Amount: $invoiceAmount"
 
     if ([string]::IsNullOrWhiteSpace($invoiceNumber)) {
-        Write-Verbose "Ignoring ${filename}"
+        Write-Verbose "Invalid data. Ignoring ${filename}"
         Return
     }
-
-    $invoiceNumberClean = ${invoiceNumber} -replace "/", "_"
 
     $index = 0;
     do {
@@ -33,24 +36,20 @@
     }
     while (Test-Path "${invoiceDate} 0${index}*")
 
-    $newFilename = "${invoiceDate} 0${index} ${documentType} SIXT share ${invoiceNumberClean}${invoiceAmount}.pdf"
+    $newFilename = "${invoiceDate} 0${index} ${documentType -not -eq $null ? "$documentType " : ""}SIXT share ${invoiceNumber}${invoiceAmount}.pdf"
+
+    Write-Verbose "OLD:    $filename"
+    Write-Verbose "NEW:    $newFilename"
 
     if (${newFilename} -eq ${filename}) {
         Write-Verbose "File has correct name. Ignoring ${filename}"
         Return
     }
 
-
-    Write-Verbose "Number: $invoiceNumber"
-    Write-Verbose "Date:   $invoiceDate"
-    Write-Verbose "Amount: $invoiceAmount"
-    Write-Verbose "OLD:    $filename"
-    Write-Verbose "NEW:    $newFilename"
-
     if ($newFilename -eq $filename) {
         return
     }
 
     Write-Output "Renaming '${filename}' to '${newFilename}'"
-    Rename-Item -WhatIf -Path "${filename}" -NewName ${newFilename}
+    Rename-Item -Path "${filename}" -NewName ${newFilename}
 }
