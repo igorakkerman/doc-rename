293adeac457869d8a890d36db9dea5ee473e6a6a
Igor Akkerman <commit@igorakkerman.de>
2022-11-10 17:09:02 +0100

    create Telekom

---
+++ Telekom/Telekom.ps1
@@ -0,0 +1,43 @@
+﻿Get-ChildItem -filter *.pdf  | Where-Object {$_.LastWriteTime -ge "2022-10-01"} | ForEach-Object <# -Parallel #> {
+    $filename = $_.Name
+    
+    $textContent = pdftotext -enc UTF-8 -layout -bom -q ${filename} - | Out-String
+
+    if (${textContent} -NotMatch "Telekom Deutschland GmbH") {
+        Write-Output "Not from Telekom. Ignoring ${filename}"
+        Return
+    }
+
+    if ( ${textContent} -cmatch "(?s).*Rechnungsnummer\s+(\d+)\s(\d+)\s(\d+)\s(\d+).*") {
+        $invoiceNumber = $matches[1] + $matches[2] + $matches[3] + $matches[4]
+    }
+    if ( ${textContent} -cmatch "(?s).*Datum\s+(\d{2})\.(\d{2}).(\d{4}).*") {
+        $invoiceDay = $matches[1]
+        $invoiceMonth = $matches[2]
+        $invoiceYear = $matches[3]
+
+        $invoiceDate = ${invoiceYear} + "-" + ${invoiceMonth} + "-" + ${invoiceDay}
+
+        Write-Output "Invoice date: $invoiceDate"
+    }
+    
+    if ( ${textContent} -cmatch "(?s).*Zu zahlender Betrag\s+(\d+,\d{2})\s*€.*") { 
+        $invoiceAmount = $matches[1]
+
+        Write-Output "Invoice amount: $invoiceAmount"
+    }
+    
+    # Write-Output "${filename}: ${invoiceNumber} ${invoiceDate} ${InvoiceAmount}"
+    if (-not ${invoiceNumber} -or -not ${invoiceDate} -or -not ${invoiceAmount}) {
+        Write-Output "Invalid invoice data. Ignoring ${filename}"
+        Return
+    }
+
+    $newFilename = "${invoiceDate} 01 Rechnung ${invoiceNumber} ${invoiceAmount}€.pdf"
+
+    Write-Output "Renaming '${filename}' to '${newFilename}'"
+    Rename-Item -Path "${filename}" -NewName ${newFilename}
+}
+
+New-Item -ItemType Directory -Force signature | Out-Null
+Move-Item *.ads signature
