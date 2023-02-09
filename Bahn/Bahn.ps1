ddd142e67ee121163c2cae3c7c0219bf3d48a72b
Igor Akkerman <commit@igorakkerman.de>
2023-02-09 16:53:35 +0100

    Refactored Bahn to rename correctly, functions inlined

--- Bahn/Bahn.ps1
+++ Bahn/Bahn.ps1
@@ -1,25 +1,23 @@
-﻿
-function Main {
-    Get-ChildItem -filter *.pdf | ForEach-Object {
-        $filename = $_.Name
+﻿Get-ChildItem -filter *.pdf | ForEach-Object {
+    $filename = $_.Name
         
-        $textContent = pdftotext.exe -layout -enc UTF-8 -bom ${filename} - | Out-String
+    $textContent = pdftotext.exe -layout -enc UTF-8 -bom ${filename} - | Out-String
     
-        Process-Document $filename
-    }    
-}
-function Process-Document($filename) {
-    if ( ${textContent} -cmatch "Datum ([0-9][0-9])\.([0-9][0-9])\.([0-9][0-9][0-9][0-9])\W+([A-Z0-9]{6})") {
+    if ( $textContent -cnotmatch "Bahn") {
+        return
+    }
+
+    if ( ${textContent} -cmatch "Datum (\d{2})\.(\d{2})\.(\d{4})\W+([A-Z0-9]{6})") {
         $invoiceYear = $matches[3]
         $invoiceMonth = $matches[2]
         $invoiceDay = $matches[1]
 
-        $invoiceDate =  ${invoiceYear} + "-" + ${invoiceMonth} + "-" + ${invoiceDay}
+        $invoiceDate = ${invoiceYear} + "-" + ${invoiceMonth} + "-" + ${invoiceDay}
 
         $ticketNumber = $matches[4]
     }
 
-    if ( ${textContent} -cmatch "Betrag\W*([0-9]+,[0-9][0-9])€") { 
+    if ( ${textContent} -cmatch "Betrag\W*(\d+,\d{2})€") { 
         $invoiceAmount = $matches[1] 
     }
     
@@ -33,22 +31,22 @@ function Process-Document($filename) {
         Return
     }
 
+    if ($filename -Match "${invoiceDate} \d{2} Ticket Bahn ${rideFrom}-${rideTo} ${ticketNumber} ${invoiceAmount}€.pdf") {
+        Write-Verbose "File has correct name: $filename"
+        Return
+    }
+
     $index = 0;
     do {
-        ${index}++
-
-        $newFilename = "${invoiceDate} 0${index} Ticket Bahn ${rideFrom}-${rideTo} ${ticketNumber} ${invoiceAmount}€.pdf"
-
-        if (${newFilename} -eq ${filename}) {
-            Write-Host "File has correct name. Ignoring ${filename}"
-            Return
-        }  
+        $index++
+        $indexTwoDigits = $('{0:d2}' -f $index)
     }
-    while (Test-Path "${invoiceDate} 0${index}*")
+    while (Test-Path "${invoiceDate} $indexTwoDigits*")
+
+    $newFilename = "${invoiceDate} $indexTwoDigits Ticket Bahn ${rideFrom}-${rideTo} ${ticketNumber} ${invoiceAmount}€.pdf"
 
     Write-Host "Renaming '${filename}' to '${newFilename}'"
     Rename-Item -Path "${filename}" -NewName ${newFilename}
- 
-}
+}    
 
-Main
+$null = Read-Host "Fertig."
